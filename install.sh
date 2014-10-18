047c90091c29d506137d0f0b29ccd413829e75a5
Julien Cordry <u0026939@linux13.scm.tees.ac.uk>
2014-10-18 16:38:20 +0100

    Added comments

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 dir=/tmp/SFML_$USER/
 
+# setup directories
 if [[ ! -d "$dir" ]]; then
     mkdir $dir
 fi
@@ -12,6 +13,8 @@ if [[ ! -d $HOME/include ]]; then
     mkdir $HOME/include
 fi
 cd $dir
+
+# libsndfile
 curl -O http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.25.tar.gz
 tar xzf libsndfile-1.0.25.tar.gz
 cd libsndfile-1.0.25
@@ -19,6 +22,8 @@ cd libsndfile-1.0.25
 make
 make install
 cd ..
+
+# glew
 curl -O http://kent.dl.sourceforge.net/project/glew/glew/1.11.0/glew-1.11.0.tgz
 tar xzf glew-1.11.0.tgz
 cd glew-1.11.0
@@ -26,6 +31,8 @@ make
 cp lib/* ~/lib
 cp -R include/GL ~/include/
 cd ../
+
+# cmake
 curl http://www.cmake.org/files/v3.0/cmake-3.0.2.tar.gz -O
 tar xfz cmake-3.0.2.tar.gz
 cd cmake-3.0.2
@@ -33,14 +40,11 @@ cd cmake-3.0.2
 make
 make install
 cd ../
+
+# SFML
 curl -O http://www.sfml-dev.org/download/sfml/2.1/SFML-2.1-sources.zip
 unzip SFML-2.1-sources.zip
 cd SFML-2.1
 cmake -DCMAKE_INSTALL_PREFIX=$HOME/
 make
 make install
-
-# g++ main.cpp -o main -I$HOME/include -L$HOME/lib/ -lsfml-graphics -lsfml-window -lsfml-system
-# 
-# http://sfml-dev.org/tutorials/2.1/start-cb.php
-# 
