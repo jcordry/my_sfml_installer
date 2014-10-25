dd9165a3289a096090f2b71969530df689f5e141
Julien Cordry <julien.cordry@gmail.com>
2014-10-26 00:41:46 +0100

    Updated the default home

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-g++ main.cpp -o main -I/home/scm-staff/u0026939/include -L/home/scm-staff/u0026939/lib/ -lsfml-graphics -lsfml-window -lsfml-system
+g++ main.cpp -o main -I$HOME/include -L$HOME/lib/ -lsfml-graphics -lsfml-window -lsfml-system
