2303017583845f7995f6fac1e8fe3370131f45ac
Julien Cordry <julien.cordry@gmail.com>
2015-01-16 10:08:29 +0000

    Added executable for codeblocks in ~/bin

--- install.sh
+++ install.sh
@@ -46,9 +46,18 @@ cd ../
 
 # SFML
 curl -O https://scm-intranet.tees.ac.uk/users/u0026939/SFML-2.1-sources.zip
-# curl -O http://www.sfml-dev.org/download/sfml/2.1/SFML-2.1-sources.zip
 unzip SFML-2.1-sources.zip
 cd SFML-2.1
 cmake -DCMAKE_INSTALL_PREFIX=$HOME/
 make
 make install
+
+# run-codeblocks.sh
+cat > ~/bin/run-codeblocks.sh << EOF
+export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH;
+codeblocks
+EOF
+chmod 755 ~/bin/run-codeblocks.sh
+cat > ~/.bash_profile << EOF
+export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH;
+EOF
